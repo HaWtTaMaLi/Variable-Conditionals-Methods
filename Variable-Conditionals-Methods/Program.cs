1f5c00e224c72de906dd86f2eacdec63442eda08
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-09-21 09:23:50 -0300

    added Experience increase

--- Variable-Conditionals-Methods/Program.cs
+++ Variable-Conditionals-Methods/Program.cs
@@ -10,24 +10,44 @@ namespace Variable_Conditionals_Methods
 
     internal class Program
     {
+        static int exp;
+        static int level;
+
         static void Main()
         {
+            //initialize
+            exp = 0;
+            level = 1;
+
             HUD();
+
+            Experience();
+
+            Console.ReadKey();
+
+            HUD();
+            
         }
 
-        static void Experience(int xp)
+        static void Experience()
         {
-
+            int xp;
+            xp = 25;
+            exp = xp + exp;
         }
 
-        static void Levels()
+        static void Level()
         {
-
+            //if exp == 25, level 2
+            //if exp == 50, level 3
+            //if exp == 
         }
 
         static void HUD()
         {
             Console.WriteLine("HUD Display");
+            Console.WriteLine("Level: " + level);
+            Console.WriteLine("XP: " + exp);
         }
     }
 }
